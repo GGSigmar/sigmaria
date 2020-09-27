369159f5598d004ff9ddeb349291afafb6ece61a
Sigmar <sigmarmalarski@gmail.com>
2020-09-27 14:25:49 +0200

    Cascade persist on entity source

--- src/Entity/Core/Traits/SourceTrait.php
+++ src/Entity/Core/Traits/SourceTrait.php
@@ -4,13 +4,14 @@ namespace App\Entity\Core\Traits;
 
 use App\Entity\Core\EntitySource;
 use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
 
 trait SourceTrait
 {
     /**
      * @var EntitySource
      *
-     * @ORM\ManyToOne(targetEntity="App\Entity\Core\EntitySource")
+     * @ORM\ManyToOne(targetEntity="App\Entity\Core\EntitySource", cascade={"persist"})
      * @Assert\Valid
      */
     private $source;
